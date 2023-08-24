58731b91e6c74860afb4a9e63e92af06af30a85e
Ali-Shahmansouri <sctshahali@gmail.com>
2023-08-24 19:13:32 +0330

    Setup React-Navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen } from "./App/screens";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
-  return <HomeScreen />;
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- frontend/App/screens/HomeScreen.tsx
+++ frontend/App/screens/HomeScreen.tsx
@@ -4,7 +4,9 @@ import React from "react";
 const HomeScreen = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500 underline font-bold text-lg">Hello World!</Text>
+      <Text className="text-red-500 underline font-bold text-lg">
+        Hello World!
+      </Text>
     </View>
   );
 };

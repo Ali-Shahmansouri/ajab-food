005013f586cb12310a651f623138848436b04ba3
Ali-Shahmansouri <sctshahali@gmail.com>
2023-08-24 18:49:49 +0330

    Setup TailwindCSS

---
+++ frontend/App/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500 underline font-bold text-lg">Hello World!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

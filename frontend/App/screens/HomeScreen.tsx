a5e95d732d84b294fd4d8788affbcd36223a90b4
Ali-Shahmansouri <sctshahali@gmail.com>
2023-08-25 18:31:49 +0330

    Implement HomeScreen navbar

--- frontend/App/screens/HomeScreen.tsx
+++ frontend/App/screens/HomeScreen.tsx
@@ -5,10 +5,17 @@ import {
   StyleSheet,
   Platform,
   StatusBar,
+  Image,
+  SafeAreaView,
+  TextInput,
+  ScrollView,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import AndroidSafeArea from "../components/AndroidSafeAreaView";
 import { fetchAllFeaturedCategories } from "../services/request";
+import { AntDesign, Entypo, MaterialIcons } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
+import AppText from "../components/AppText";
 
 const HomeScreen = () => {
   const [featuredCategories, setFeaturedCategories] = useState<
@@ -30,11 +37,60 @@ const HomeScreen = () => {
   }
 
   return (
-    <View style={AndroidSafeArea}>
-      <Text className="text-lg font-bold text-red-500 underline">
-        Hello World!
-      </Text>
-    </View>
+    <SafeAreaView style={AndroidSafeArea} className="bg-white">
+      <View className="flex-row items-center justify-between px-4 py-3">
+        <View>
+          <AntDesign name="user" size={35} color="#00CCBB" />
+        </View>
+        <View className="flex-row items-center gap-2">
+          <View>
+            <AppText Classes="text-xs font-vazirBold text-gray-400">
+              هم اکنون سفارش دهید!
+            </AppText>
+            <View className="flex-row items-center justify-center">
+              <View>
+                <Entypo name="chevron-down" size={20} color="#00CCBB" />
+              </View>
+              <AppText Classes="text-lg font-vazirBold font-">
+                موقیت کنونی شما
+              </AppText>
+            </View>
+          </View>
+          <Image
+            className="h-7 w-7 rounded-full p-4"
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+          />
+        </View>
+      </View>
+
+      <View className="flex-row items-center gap-x-2 px-4 pb-2">
+        <View>
+          <MaterialIcons
+            name="settings-input-component"
+            size={25}
+            color="#00CCBB"
+          />
+        </View>
+        <View className="flex-1 flex-row items-center justify-end gap-x-2 bg-gray-200 py-2 pr-2">
+          <TextInput
+            placeholder="رستوران ها و آشپزخانه ها "
+            className="font-vazir flex-1"
+            textAlign="right"
+            keyboardType="default"
+          />
+          <View className="">
+            <AntDesign name="search1" size={24} color="gray" />
+          </View>
+        </View>
+      </View>
+
+      {/* Body */}
+      <ScrollView>
+        
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
